20279311514e2f3a2e8d3d43e2782687d203d683
naototanabe <kotikame00@gmail.com>
2022-03-16 19:35:58 +0900

    migrate

--- routes/web.php
+++ routes/web.php
@@ -27,4 +27,9 @@ Route::get('hello/add', 'HelloController@add');
 Route::post('hello/add', 'HelloController@create');
 
 Route::get('hello/edit', 'HelloController@edit');
-route::post('hello/edit', 'HelloController@update');
+Route::post('hello/edit', 'HelloController@update');
+
+Route::get('hello/del', 'HelloController@del');
+Route::post('hello/del', 'HelloController@remove');
+
+Route::get('hello/show', 'HelloController@show');
